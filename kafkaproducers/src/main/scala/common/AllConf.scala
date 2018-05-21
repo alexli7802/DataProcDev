1f783a72fd4f4650ec9ba59277a6b8181f9a4c59
Alex Li <ali@cellossoftware.com>
2018-05-21 12:01:57 +1000

    avro consumer

--- kafkaproducers/src/main/scala/common/AllConf.scala
+++ kafkaproducers/src/main/scala/common/AllConf.scala
@@ -7,6 +7,8 @@ object AllConf {
   lazy val cfg = ConfigFactory.load
   
   def getString(p: String) = cfg.getString(p)
+  def getInteger(p: String) = cfg.getInt(p)
+  def getBoolean(p: String) = cfg.getBoolean(p)
   
   def main(args: Array[String]): Unit = {
     
--- kafkaproducers/src/main/scala/consumers/AvroConsumer.scala
+++ kafkaproducers/src/main/scala/consumers/AvroConsumer.scala
@@ -6,12 +6,15 @@ import org.apache.kafka.clients.consumer.{KafkaConsumer,ConsumerRecord}
 import org.apache.kafka.common.TopicPartition
 import scala.collection.JavaConversions._
 import scala.collection.JavaConverters._
+import cellos.messages.avro.gtpu_report
 
 case class PartitionStatus(part: TopicPartition, sOffset: Long, eOffset: Long, lstCommitted: Long) {
   override def toString = {
     val len = eOffset - sOffset
     s"${part.topic()}:${part.partition()}: len=$len, [$sOffset - $eOffset], committed-at: $lstCommitted"
   }
+  
+  def messageCount = eOffset - sOffset
 }
 
 object AvroConsumer {
@@ -36,37 +39,57 @@ object AvroConsumer {
     
   } 
  
+ 
+  def partitionStatus(parts: List[TopicPartition]): List[PartitionStatus] = {
+    val b_offsets = consumer.beginningOffsets(parts)
+    val e_offsets = consumer.endOffsets(parts)
+    parts.map(p => {
+      val lstCommitted = consumer.committed(p)
+      val cmtOffset = if (lstCommitted == null) 0 else lstCommitted.offset()
+      PartitionStatus(p, b_offsets(p), e_offsets(p), cmtOffset)
+    })
+  }
+    
+  def singlePoll(): Unit = {
+
+    val poll_ms = common.AllConf.getInteger("kafkaCluster.consumer.timeouts.poll_ms")
+    val cdrs = consumer.poll(poll_ms)     // timeout for poll = 100ms
+		println(s"consumer polls ${cdrs.count()} messages --->")
+      
+    // batch-processing
+		val (fst, lst) = {
+      val l = cdrs.iterator().toList
+      (l.head, l.last)
+    }
+    
+    println(s"\t${fst.topic()}-${fst.partition()} [offset=${fst.offset()}]: " + gtpu_report.fromByteBuffer(fst.value()))              
+    println("\t... ...")
+    println(s"\t${lst.topic()}-${lst.partition()} [offset=${lst.offset()}]: " + gtpu_report.fromByteBuffer(lst.value()))              
+
+    cdrs.foreach(cdr => {
+      val msg = gtpu_report.fromByteBuffer(cdr.value())
+//      println(s"${cdr.topic()}-${cdr.partition()} [offset=${cdr.offset()}]: " + msg)              
+    })
+   
+    consumer.commitSync()
+  }
   
-  
+  // application entries here ----->
   def main(args: Array[String]): Unit = {
     
     val topic = common.AllConf.getString("kafkaCluster.topics.gtpu")
-
-    val allTopics = consumer.listTopics().keySet().filterNot(_.startsWith("__"))
+    val tparts = consumer.partitionsFor(topic).map(p => new TopicPartition(p.topic(), p.partition())).toList
+    consumer.assign(tparts)
     
-    allTopics.foreach { t => 
-      
-      val tparts = consumer.partitionsFor(t)
-                    .map(p => new TopicPartition(p.topic(), p.partition()) )
-                    .toList
-      if (t == topic) consumer.assign(tparts)
-      
-      val stats = {
-        val b_offsets = consumer.beginningOffsets(tparts)
-        val e_offsets = consumer.endOffsets(tparts)
-        
-        tparts.map(p => {
-          val cmted = consumer.committed(p)
-          val cmtOffset = if (cmted == null) 0 else cmted.offset() 
-          PartitionStatus(p, b_offsets(p), e_offsets(p), cmtOffset)
-        })
-      }
-
-      val totalCnt = stats.map(s => (s.eOffset - s.sOffset)).sum
-      
-      println(s"[$t] overall: $totalCnt / ${stats.length}")
-      stats.foreach(s => println("\t" + s.toString))
-    }
+    val fromBegin = common.AllConf.getBoolean("kafkaCluster.consumer.startover")
+    if (fromBegin) consumer.seekToBeginning(tparts)
+    
+    val partStats = partitionStatus(tparts)
+    println(s"[$topic] overall: ${partStats.map(_.messageCount).sum} / ${partStats.length}")
+    partStats.foreach(s => println("\t" + s.toString))
+    
+    //consuming
+    singlePoll()
   }
   
 }
