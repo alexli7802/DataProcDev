fa124c04b526e57e22c485d1864b2167f168c764
Alex Li <ali@cellossoftware.com>
2018-05-22 11:33:23 +1000

    updated .gitignore

--- sparkproj/project/Dependencies.scala
+++ sparkproj/project/Dependencies.scala
@@ -2,4 +2,11 @@ import sbt._
 
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
+  lazy val sparkVer = "2.3.0"
+  lazy val sparkDeps = Seq(
+      "org.apache.spark" %% "spark-core" % sparkVer % "provided",
+      "org.apache.spark" %% "spark-sql" % sparkVer % "provided"
+    )
 }
+
+
