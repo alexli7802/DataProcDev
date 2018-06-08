8d8de6c57231bc1a9f5859d4d5cdf30bccec6ce2
alex.li <alex.xin.li@hotmail.com>
2018-06-08 14:40:23 +1000

    added poc.avro/protobuf

---
+++ sparkproj/src/main/scala/poc/avro/BasicTest.scala
@@ -0,0 +1,25 @@
+package poc.avro
+
+/*	setup:
+ * 	1. create '???' according to 'testdata.Employee'
+ * 	2. compile '???' to generate java classes
+ *  3. convert 'testdata.Employee' into 'avro object'
+ *  
+ *  4a. write 'avro object' to disk-file
+ *  /
+ * 	4b. read disk-file then parse 'avro' into 'testdata.Employee'
+ * 
+ * 
+ *  run:
+ *  java -cp path/to/???.jar poc.avro.BasicTest
+ *  
+ * */
+
+
+object BasicTest {
+  
+  
+  def main(args: Array[String]): Unit = {
+    println("============== poc.avro.BasicTest ===============")
+  }
+}
---
+++ sparkproj/src/main/scala/poc/protobuf/BasicTest.scala
@@ -0,0 +1,25 @@
+package poc.protobuf
+
+
+/*	setup:
+ * 	1. create 'Employee.pb' according to 'testdata.Employee'
+ * 	2. compile 'Employee.pb' to generate java classes
+ *  3. convert 'testdata.Employee' into 'protobuf object'
+ *  
+ *  4a. write 'protobuf object' to disk-file
+ *  /
+ * 	4b. read disk-file then parse 'protobuf' into 'testdata.Employee'
+ *
+ *  run:
+ *  java -cp path/to/???.jar poc.protobuf.BasicTest
+ * 
+ * */
+
+
+object BasicTest {
+  
+  
+  def main(args: Array[String]): Unit = {
+    println("============== poc.protobuf.BasicTest ===============")
+  }
+}
